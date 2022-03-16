91cd9e5b07bd1f84f1daa4263c88ad5b2a020b4f
peterphilips <peter.philips@surroundaustralia.com>
2022-03-17 09:20:41 +1100

    Switched FEATURE_TYPES to URIREF and changed g.add foi to include said URIRef's

--- utils.py
+++ utils.py
@@ -10,13 +10,36 @@ BDRM = Namespace("https://linked.data.gov.au/def/bdr-msg/")
 GEO = Namespace("http://www.opengis.net/ont/geosparql#")
 # Feature Types vocab http://linked.data.gov.au/def/tern-cv/68af3d25-c801-4089-afff-cf701e2bd61d
 # Method Types vocab http://linked.data.gov.au/def/tern-cv/9b6e057f-271b-48f6-8c33-0528bf6b60df
-FEATURE_TYPES = ["administrative_area", "animal_individual", "animal_occurrence",
-                 "animal_population", "animal_specimen", "climate", "fungal_occurrence",
-                 "geologic_substrate", "habitat", "land_surface_substrate", "landform",
-                 "soil_profile", "soil_sample", "soil_specimen", "individual_stem",
-                 "plant_specimen", "vegetation_stand", "plant_community", "plant_litter",
-                 "plant_occurrence", "plant_population", "vegetation_disturbance",
-                 "vegetation_fuel", "plant_taxa_per_stratum"]
+FEATURE_TYPES = [URIRef("http://linked.data.gov.au/def/tern-cv/05dac53a-269c-4699-9673-bf99a9406b14"),
+                 # administrative_area
+                 URIRef("http://linked.data.gov.au/def/tern-cv/ecb855ed-50e1-4299-8491-861759ef40b7"),
+                 # animal Individuals
+                 URIRef("http://linked.data.gov.au/def/tern-cv/2361dea8-598c-4b6f-a641-2b98ff199e9e"),
+                 # animal occurence
+                 URIRef("http://linked.data.gov.au/def/tern-cv/8a68b4a9-167b-40f0-9222-293a2d20ffee"),
+                 # animal population
+                 URIRef("http://linked.data.gov.au/def/tern-cv/cd5cbdbb-07d9-4a5b-9b11-5ab9d6015be6"),
+                 # animal specimen
+                 URIRef("http://linked.data.gov.au/def/tern-cv/6d40d71e-58cd-4f75-8304-40c01fe5f74c"),  # climate
+                 URIRef("http://linked.data.gov.au/def/tern-cv/45a73139-f6bf-47b7-88d4-4b2865755545"),
+                 # fungal occurence
+                 URIRef("http://linked.data.gov.au/def/tern-cv/afc81cca-9122-4e36-823d-31dd765e9257"),
+                 # Geologic substrate
+                 URIRef("http://linked.data.gov.au/def/tern-cv/2090cfd9-8b6b-497b-9512-497456a18b99"),  # habitat
+                 URIRef("http://linked.data.gov.au/def/tern-cv/7e256d28-e686-4b6a-b64a-ac1b1a8f164d"),
+                 # land surface disturbance
+                 URIRef("http://linked.data.gov.au/def/tern-cv/aef12cd6-3826-4988-a54c-8578d3fb4c8d"),
+                 # land surface substrate
+                 URIRef("http://linked.data.gov.au/def/tern-cv/2cf3ed29-440e-4a50-9bbc-5aab30df9fcd"),  # landform
+                 URIRef("http://linked.data.gov.au/def/tern-cv/80c39b95-0912-4267-bb66-2fa081683723"),  # soil profile
+                 URIRef("http://linked.data.gov.au/def/tern-cv/06461021-a6c2-4175-9651-23653c2b9116"),  # soil sample
+                 URIRef("http://linked.data.gov.au/def/tern-cv/d738a3f9-9b00-4adf-9dc8-0577269b691d"),  # soil Specimen
+                 URIRef("http://linked.data.gov.au/def/tern-cv/60d7edf8-98c6-43e9-841c-e176c334d270"),
+                 # plant individual
+                 URIRef("http://linked.data.gov.au/def/tern-cv/2e122e23-881c-43fa-a921-a8745f016ceb"),
+                 # plant specimen
+                 ]
+
 METHOD_TYPES = ["HCL_droplets_1_ML", "densitometer", "dgps",
                 "electrical_conductivity_meter", "ph_meter", "point_intercept",
                 "soil_core", "soil_pit", "visual_assessment", "basal_wedge",
@@ -24,6 +47,13 @@ METHOD_TYPES = ["HCL_droplets_1_ML", "densitometer", "dgps",
                 "human_measurement", "human_observation", "instrument_measurement",
                 "munsell_soil_colour_chart", "soil_10_cm_samples",
                 "soil_auger_boring", "soil_vertical_exposure", "vertex_hypsometer"]
+
+
+def validate_number(n):
+    if n < 0 or n > 10000:
+        print("Data number has to be greater than 0 and less than 10000")
+        return False
+    return True
 
 
 def random_char(y):
@@ -71,6 +101,10 @@ def create_sampling_data(n):
 
 
 def create_dataset(n):
+    if validate_number(n) != False:
+        pass
+    else:
+        exit(1)
     # Prefix and base URI creation
     g = Graph(base="https://linked.data.gov.au/dataset/bdr/")
     g.bind("bdrm", BDRM)
@@ -100,14 +134,8 @@ def create_dataset(n):
         # + "/foi_number/" + str(index + 1))
         foi_list.append(foi)
         g.add((foi, RDF.type, TERN.FeatureOfInterest))
-        g.add((foi, TERN.featureType, Literal(random.choice(FEATURE_TYPES))))
+        g.add((foi, TERN.featureType, random.choice(FEATURE_TYPES)))
 
-        # I NEED TO COME BACK TO THIS PROBLEM -> integrating fois into ds
-        # can probs do some mathematical formula for this.
-        #  0 - 200 -> dataset 1, 200 - 400, dataset 2, 400-600, dataset 3 ...
-        # 0 - 50 -> foi 1, 50 - 100 -> foi 2 ....
-        # every 4 fois swtich up
-        # 1 - 4 -> d1, 5-8 -> d2 etc
         # integrating foi into associated ds
         x = int(index / 4) + 1
         ds = URIRef("http://example.com/dummy/dataset/" + str(x))
@@ -139,7 +167,7 @@ def create_dataset(n):
         # link Sample to Sampling
         g.add((sampling_iri, SOSA.hasResult, sample_iri))
 
-    print(g.serialize())
+    print(g.serialize(destination="test.ttl"))
 
 
-create_dataset(2)
+create_dataset(10000)
