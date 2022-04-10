c2d3b1b8fec5b0c8d783c32bbcc1f7e90c0bc6ec
nicholascar <nicholas.car@surroundaustralia.com>
2022-04-10 23:21:24 +1000

    rename dataset files as per recipes

--- make_all.sh
+++ make_all.sh
@@ -1,4 +1,5 @@
-python client broad 100 > test_client_broad_dataset.ttl
-python client narrow 100 > test_client_narrow_dataset.ttl
-python client degree 100 > test_client_degree_dataset.ttl
-python client embargoed 1 > test_client_embargoed_dataset.ttl
+python client broad 100 > test_client_broad.ttl
+python client narrow 100 > test_client_narrow.ttl
+python client degree 100 > test_client_degree.ttl
+python client embargoed > test_client_embargoed.ttl
+python client conservation > test_client_conservation.ttl
