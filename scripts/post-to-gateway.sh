e35428b88cbad4c8446299bddf9aebc41cc1e7d0
Nicholas Car <nick@kurrawong.net>
2022-05-20 17:16:06 +1000

    scaling data

---
+++ scripts/post-to-gateway.sh
@@ -0,0 +1,11 @@
+time curl --data-binary @scaling/scale_500.ttl http://gateway.dev.bdr.gov.au/absorb
+
+time curl --data-binary @scaling/scale_1000.ttl http://gateway.dev.bdr.gov.au/absorb
+
+time curl --data-binary @scaling/scale_2000.ttl http://gateway.dev.bdr.gov.au/absorb
+
+time curl --data-binary @scaling/scale_3000.ttl http://gateway.dev.bdr.gov.au/absorb
+
+time curl --data-binary @scaling/scale_4000.ttl http://gateway.dev.bdr.gov.au/absorb
+
+time curl --data-binary @scaling/scale_5000.ttl http://gateway.dev.bdr.gov.au/absorb
